8db38ab66f16eed707aa8ca6d480784303dacc2e
roblesdotdev <httpsnip@gmail.com>
2025-06-26 21:10:08 -0300

    add examples

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,83 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import gsap from "gsap";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// triggers when the initial HTML is fully loaded.
+document.addEventListener("DOMContentLoaded", () => {
+  /* TWEENS */
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  // Example 1:
+
+  // to("selector", objProps)
+  const tweenTo = gsap.to(".box", {
+    x: 300,
+    duration: 3,
+    rotate: 90,
+    ease: "power1",
+
+    // infinite
+    repeat: -1,
+    yoyo: true,
+  });
+
+  document.getElementById("play")?.addEventListener("click", () => {
+    tweenTo.play();
+  });
+  document.getElementById("#pause")?.addEventListener("click", () => {
+    tweenTo.pause();
+  });
+  document.getElementById("#resume")?.addEventListener("click", () => {
+    tweenTo.resume();
+  });
+  document.getElementById("#reverse")?.addEventListener("click", () => {
+    tweenTo.reverse();
+  });
+  document.getElementById("#restart")?.addEventListener("click", () => {
+    tweenTo.restart();
+  });
+
+  // Example 2
+
+  // fromTo("selector", startObjProps, endObjProps)
+  gsap.fromTo(
+    ".box2",
+    {
+      opacity: 0.2,
+      x: 0,
+    },
+    {
+      opacity: 1,
+      x: 300,
+      duration: 3,
+
+      // Infinite
+      repeat: -1,
+      yoyo: true,
+    }
+  );
+
+  /* TIMELINE */
+  const tl = gsap.timeline();
+  tl.to(".item1", {
+    x: 300,
+    borderRadius: "50%",
+    duration: 2,
+    repeat: -1,
+    yoyo: true,
+  });
+  tl.to(
+    ".item2",
+    { x: 300, rotate: 90, duration: 2, repeat: -1, yoyo: true },
+    "-=1.8"
+  );
+  tl.to(
+    ".item3",
+    {
+      x: 300,
+      background: "transparent",
+      border: "10px solid black",
+      duration: 2,
+      repeat: -1,
+      yoyo: true,
+    },
+    "-=1.6"
+  );
+});
